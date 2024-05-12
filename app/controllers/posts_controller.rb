7c28dde7e3947efc0d4c0717e61ee5460e80d5c3
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-05-12 17:46:52 +0000

    guardado7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,17 @@ class PostsController < ApplicationController
     # render posts/new view with new Post form
   end
 
-  # def create
+  def create
   #   # start with a new Post
+    @new_row_posts = Post.new
   #   # assign user-entered form data to Post's columns
+    @new_row_posts["author"] = params["author"]
+    @new_row_posts["image"] = params["image"]
+    @new_row_posts["body"] = params["body"]
   #   # save Post row
+  @new_row_posts.save
   #   # redirect user
-  # end
+  redirect_to "/posts"
+  end
 
 end
